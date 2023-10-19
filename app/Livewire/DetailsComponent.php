9a3ee36917e0e7b2d0248e414bcba1d9c49c797e
Vape Center <mdnaimul15902@gmail.com>
2023-10-19 18:01:18 +0400

    add to cart from wishlist & Add to card quantity product

--- app/Livewire/DetailsComponent.php
+++ app/Livewire/DetailsComponent.php
@@ -10,17 +10,33 @@ use Cart;
 class DetailsComponent extends Component
 {
     public $slug;
+    public $qty;
+
 
     public function mount($slug)
     {
         $this->slug = $slug;
+        $this->qty = 1;
     }
 
     public function store($product_id,$product_name,$product_price)
     {
-        Cart::instance('cart')->add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        Cart::instance('cart')->add($product_id,$product_name,$this->qty,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in Cart');
         return redirect()->route('cart');
+    }
+
+    public function increasQuantity()
+    {
+        $this->qty++;
+    }
+
+    public function decreasQuantity()
+    {
+        if($this->qty > 1)
+        {
+            $this->qty--;
+        }
     }
 
     public function render()
--- app/Livewire/WishlistComponent.php
+++ app/Livewire/WishlistComponent.php
@@ -20,6 +20,15 @@ class WishlistComponent extends Component
         }
     }
 
+    public function moveProductWislistToCart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id,$item->name,1,$item->price)->associate('App\Models\Product');
+        $this->dispatch('refreshComponent')->to('wishlist-count-component ');
+        $this->dispatch('refreshComponent')->to('cart-count-component ');
+    }
+
     public function render()
     {
         return view('livewire.wishlist-component')->layout('layouts.app');
