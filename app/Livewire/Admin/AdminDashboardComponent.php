6df453ec6b45f7713c077c63d973af73b8d0858a
Vape Center <mdnaimul15902@gmail.com>
2023-11-04 18:48:04 +0400

    Show Data In Admin Dashbord

--- app/Livewire/Admin/AdminDashboardComponent.php
+++ app/Livewire/Admin/AdminDashboardComponent.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Order;
+use Carbon\Carbon;
 
 class AdminDashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.admin-dashboard-component')->layout('layouts.app');
+        $orders = Order::orderBy("created_at","desc")->paginate(10);
+        $totalSales = Order::where('status','delivered')->count();
+        $totalRevenus = Order::where('status','delivered')->sum('total');
+        $todaySales = Order::where('status','delivered')->whereDate('created_at',Carbon::today())->count();
+        $todayRevenus = Order::where('status','delivered')->whereDate('created_at',Carbon::today())->sum('total');
+        
+        return view('livewire.admin.admin-dashboard-component',[
+            'orders' => $orders,
+            'totalSales' => $totalSales,
+            'totalRevenus' => $totalRevenus,
+            'todaySales' => $todaySales,
+            'todayRevenus' => $totalRevenus,
+        ])->layout('layouts.app');
     }
 }
